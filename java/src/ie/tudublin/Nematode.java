09e5c2f961cf9af8e4525c4f55c6938f86df2203
aidan959 <6438565+aidan959@users.noreply.github.com>
2022-04-27 18:36:28 +0100

    Finished code submissions and cleaned up issues

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -6,7 +6,7 @@ import processing.core.PApplet;
 import processing.data.TableRow;
 
 public class Nematode {
-    public Nematode(String name, int length, Boolean limbs, Gender gender, boolean eyes,PApplet pa) {
+    public Nematode(String name, int length, Boolean limbs, Gender gender, boolean eyes, PApplet pa) {
         setName(name);
         setLength(length);
         setLimbs(limbs);
@@ -14,134 +14,153 @@ public class Nematode {
         setEyes(eyes);
         this.pa = pa;
     }
-    public Nematode(TableRow tr, PApplet pa){
-        
-        this(tr.getString("name"), tr.getInt("length"), tr.getInt("limbs") == 1, Gender.valueOf(tr.getString("gender")), tr.getInt("eyes") == 1, pa);
+
+    public Nematode(TableRow tr, PApplet pa) {
+
+        this(tr.getString("name"), tr.getInt("length"), tr.getInt("limbs") == 1, Gender.valueOf(tr.getString("gender")),
+                tr.getInt("eyes") == 1, pa);
     }
-    public enum Gender{
+
+    public enum Gender {
         m,
         f,
         h,
         n
     }
+
     private String name;
     private int length;
     private boolean limbs;
     private Gender gender;
     private boolean eyes;
     private PApplet pa;
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public int getLength() {
         return length;
     }
+
     public void setLength(int length) {
         this.length = length;
     }
+
     public boolean getLimbs() {
         return limbs;
     }
+
     public void setLimbs(boolean limbs) {
         this.limbs = limbs;
     }
+
     public boolean getEyes() {
         return eyes;
     }
+
     public void setEyes(boolean eyes) {
         this.eyes = eyes;
     }
+
     public Gender getGender() {
         return gender;
     }
+
     public void setGender(Gender gender) {
         this.gender = gender;
     }
-    
+
     Vector center;
-    float size = 50; 
+    float size = 50;
     float extremityLength = 15;
     private float movementX = 0;
-    public float getMovementX(){
+
+    public float getMovementX() {
         return movementX;
     }
-    public void setMovementX(float movementX){
+
+    public void setMovementX(float movementX) {
         this.movementX = movementX;
     }
-    public void render(){
+
+    public void render() {
         float x = pa.width / 2f;
-        float y = ((pa.height - (size * length))/2);
+        float y = ((pa.height - (size * length)) / 2);
         pa.pushMatrix();
-        
+
         pa.textSize(20);
         pa.textAlign(PApplet.CENTER);
-        
-        pa.strokeWeight(3);
-        pa.translate(x, y + size/2);
-        pa.translate(movementX, 0);
-        pa.text(name, 0,  -size/2);
-        pa.noFill();
-        for(int i = 0; i < length; i++){
-            pa.translate(0, size);
-            pa.stroke(pa.frameCount % 360 + 5*i, 150, 100);
 
-            if(eyes && i == 0){
-                float height = PApplet.sin(45) * (size/2) - 5;
-                float width = size/2 - 5;
+        pa.strokeWeight(3);
+        pa.translate(x, y + size / 2);
+        pa.translate(movementX, 0);
+        pa.text(name, 0, -size / 2);
+        pa.noFill();
+        for (int i = 0; i < length; i++) {
+            pa.translate(0, size);
+            pa.stroke(pa.frameCount % 360 + 5 * i, 150, 100);
+
+            if (eyes && i == 0) {
+                float height = PApplet.sin(45) * (size / 2) - 5;
+                float width = size / 2 - 5;
                 pa.line(-width, -height, (-width) - extremityLength, -height - extremityLength);
-                pa.circle(-width - extremityLength- size/8, -height - extremityLength - size/8, size/4);
-                pa.stroke(pa.frameCount % 360+(10*i), 150, 100);
+                pa.circle(-width - extremityLength - size / 8, -height - extremityLength - size / 8, size / 4);
+                pa.stroke(pa.frameCount % 360 + (10 * i), 150, 100);
 
                 pa.line(width, -height, (width) + extremityLength, -height - extremityLength);
-                pa.stroke(pa.frameCount % 360+(20*i), 150, 100);
+                pa.stroke(pa.frameCount % 360 + (20 * i), 150, 100);
 
-                pa.circle(width + extremityLength + size/8, -height - extremityLength - size/8, size/4);
-                
+                pa.circle(width + extremityLength + size / 8, -height - extremityLength - size / 8, size / 4);
+
             }
             pa.circle(0, 0, size);
-            if(limbs){
-                pa.stroke(pa.frameCount % 360-(10*i), 150, 100);
+            if (limbs) {
+                pa.stroke(pa.frameCount % 360 - (10 * i), 150, 100);
 
-                pa.line(-size/2, 0, (-size/2) -30, 0);
-                pa.line(size/2, 0, (size/2) + 30, 0);
+                pa.line(-size / 2, 0, (-size / 2) - 30, 0);
+                pa.line(size / 2, 0, (size / 2) + 30, 0);
 
             }
-            
+
         }
-        
-        switch (gender){
+
+        switch (gender) {
             case m:
-            pa.line(0, (size/2), 0, (size/2) + extremityLength);
-            pa.circle(0, (size/2) + extremityLength + size/8, size/4);
-            break;
+                pa.line(0, (size / 2), 0, (size / 2) + extremityLength);
+                pa.circle(0, (size / 2) + extremityLength + size / 8, size / 4);
+                break;
             case f:
-            pa.circle(0, 0, (size*2)/4);
-            break;
+                pa.circle(0, 0, (size * 2) / 4);
+                break;
             case h:
-            pa.circle(0, 0, (size*2)/4);
-            break;
+                pa.circle(0, 0, (size * 2) / 4);
+                break;
             case n:
-            break;
+                break;
 
         }
         pa.popMatrix();
     }
-    public String toString(){
+
+    public String toString() {
         return "Nematode " + name + ", length: " + length + ", limbs: " + limbs + ", gender: " + evalGender();
     }
-    public String evalGender(){
-        switch (gender){
+
+    public String evalGender() {
+        switch (gender) {
             case m:
-            return "male";
+                return "male";
             case f:
-            return "female";
+                return "female";
             case h:
-            return "hermaphrodite";
+                return "hermaphrodite";
             case n:
-            return "no-gender";
+                return "no-gender";
         }
         return gender.toString();
     }
--- java/src/ie/tudublin/NematodeVisualiser.java
+++ java/src/ie/tudublin/NematodeVisualiser.java
@@ -6,28 +6,23 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 
-public class NematodeVisualiser extends PApplet
-{
+public class NematodeVisualiser extends PApplet {
 
-	public void keyPressed()
-	{	
-		if(changingNematode){
+	public void keyPressed() {
+		if (changingNematode) {
 
-		}	
-		else if(keyCode == LEFT){
+		} else if (keyCode == LEFT) {
 			previousNematode = currentNematode;
 			currentNematode--;
-			if(currentNematode < 0){
-				currentNematode = nematodes.size() -1;
+			if (currentNematode < 0) {
+				currentNematode = nematodes.size() - 1;
 			}
 			changingNematode = true;
 			changeFrame = true;
-		}	
-		else if (keyCode == RIGHT)
-		{
+		} else if (keyCode == RIGHT) {
 			previousNematode = currentNematode;
 			currentNematode++;
-			if(currentNematode > nematodes.size()-1){
+			if (currentNematode > nematodes.size() - 1) {
 				currentNematode = 0;
 			}
 			changingNematode = true;
@@ -36,103 +31,102 @@ public class NematodeVisualiser extends PApplet
 		}
 	}
 
-
-	public void settings()
-	{
+	public void settings() {
 		size(800, 800);
 	}
 
-	public void setup() 
-	{
+	public void setup() {
 		colorMode(HSB);
 		background(0);
-		smooth();	
-		loadNematodes();			
+		smooth();
+		loadNematodes();
 	}
+
 	ArrayList<Nematode> nematodes;
 	int currentNematode;
 	int previousNematode;
 	boolean changeFrame;
-	public void loadNematodes()
-	{
+
+	public void loadNematodes() {
 		nematodes = new ArrayList<>();
 		Table table;
 		table = loadTable("data/nematodes.csv", "header");
-		
-		for(TableRow row : table.rows()){
+
+		for (TableRow row : table.rows()) {
 			nematodes.add(new Nematode(row, this));
 
 		}
 
-		for(Nematode nematode: nematodes){
+		for (Nematode nematode : nematodes) {
 			System.out.println(nematode);
 		}
 	}
+
 	boolean changingNematode;
 
-	public void draw()
-	{	
+	public void draw() {
 		clear();
 		Nematode nematode = nematodes.get(currentNematode);
 		Nematode previousNema = null;
-		if(changingNematode){
+		if (changingNematode) {
 			previousNema = nematodes.get(previousNematode);
-			if(changeFrame){
+			if (changeFrame) {
 				changeFrame = false;
-				if(currentNematode> previousNematode){
-					nematode.setMovementX(width/2);
+				if (currentNematode > previousNematode) {
+					nematode.setMovementX(width / 2);
 					previousNema.setMovementX(0);
 				} else {
-					nematode.setMovementX(-width/2);
+					nematode.setMovementX(-width / 2);
 					previousNema.setMovementX(0);
 
 				}
 			}
-			if(currentNematode > previousNematode){
+			if (currentNematode > previousNematode) {
 				nematode.setMovementX(nematode.getMovementX() - 5);
 				previousNema.setMovementX(previousNema.getMovementX() - 5);
-				
-				if(previousNema.getMovementX() < -width/2){
+
+				if (previousNema.getMovementX() < -width / 2) {
 					nematode.setMovementX(0);
 					changingNematode = false;
 				}
-			} else{
-				nematode.setMovementX(nematode.getMovementX()+5);
+			} else {
+				nematode.setMovementX(nematode.getMovementX() + 5);
 				previousNema.setMovementX(previousNema.getMovementX() + 5);
-				
-				if(previousNema.getMovementX() > width/2){
+
+				if (previousNema.getMovementX() > width / 2) {
 					nematode.setMovementX(0);
-					
+
 					changingNematode = false;
 				}
-				
+
 			}
 		}
 		nematode.render();
-		if(previousNema != null){
+		if (previousNema != null) {
 			previousNema.render();
 		}
-		drawArrow(width/4, height/2, 50, true);
-		drawArrow(3* width/4, height/2, 50, false);
-		if(frameCount % 60 == 0)
+		drawArrow(width / 4, height / 2, 50, true);
+		drawArrow(3 * width / 4, height / 2, 50, false);
+		if (frameCount % 60 == 0)
 			System.out.println(nematodes.get(currentNematode));
 	}
-	public void drawArrow(int x, int y, float len, boolean left){
+
+	public void drawArrow(int x, int y, float len, boolean left) {
 		pushMatrix();
 		translate(x, y);
 		stroke(360, 360, 360);
 		strokeWeight(4);
 
-		if(left){
-			line(0,0, -len, 0);
+		if (left) {
+			line(0, 0, -len, 0);
 			line(-len, 0, -len + 8, +8);
- 			line(-len, 0, -len + 8, -8);
+			line(-len, 0, -len + 8, -8);
 		} else {
-			line(0,0, len, 0);
+			line(0, 0, len, 0);
 			line(len, 0, len - 8, +8);
- 			line(len, 0, len - 8, -8);
+			line(len, 0, len - 8, -8);
 		}
 		popMatrix();
 	}
-	
+
 }
